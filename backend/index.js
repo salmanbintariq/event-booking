f67a2a08ad6e5386517a65e1509e141611a935ba
Devops <salmantariq8018@gmail.com>
2026-09-02 13:11:05 +0500

    feat: add event and booking routes

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,8 @@ const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const connectDB = require("./config/db");
 const authRoutes = require("./routes/authRoutes");
+const eventRoutes = require("./src/routes/eventRoutes");
+const bookingRoutes = require("./src/routes/bookingRoutes");
 
 dotenv.config();
 
@@ -16,6 +18,8 @@ app.use(cookieParser());
 
 // Routes
 app.use("/api/auth", authRoutes);
+app.use("/api/events", eventRoutes);
+app.use("/api/bookings", bookingRoutes);
 
 
 
