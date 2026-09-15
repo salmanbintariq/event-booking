8e26c568dff7e7dd07387133e81ec2591d35d5e0
Devops <salmantariq8018@gmail.com>
2026-09-15 14:00:02 +0500

    feat: add cloudinary image upload

---
+++ backend/src/utils/cloudinaryUpload.js
@@ -0,0 +1,24 @@
+const cloudinary = require("../config/cloudinary");
+
+const uploadToCloudinary = (fileBuffer) => {
+  return new Promise((resolve, reject) => {
+    const uploadStream = cloudinary.uploader.upload_stream(
+      {
+        folder: "event-booking/events",
+        resource_type: "image",
+      },
+      (error, result) => {
+        if (error) {
+          reject(error);
+          return;
+        }
+
+        resolve(result);
+      }
+    );
+
+    uploadStream.end(fileBuffer);
+  });
+};
+
+module.exports = uploadToCloudinary;
